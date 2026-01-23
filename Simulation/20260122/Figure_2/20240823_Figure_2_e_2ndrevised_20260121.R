f0bd668d102b363be463ea36298d650ac38c5dd8
Hiroki Ando <hirokiando.res@gmail.com>
2026-01-23 00:28:21 -0700

    Update 20240823_Figure_2_e_2ndrevised_20260121.R

--- Simulation/20260122/Figure_2/20240823_Figure_2_e_2ndrevised_20260121.R
+++ Simulation/20260122/Figure_2/20240823_Figure_2_e_2ndrevised_20260121.R
@@ -1,12 +1,12 @@
 setwd("C:/WBE_ND")
 
-library(deSolve)
 library(dplyr)
 library(tidyr)
 library(plyr)
 library(ggplot2)
 library(rstan)
 library(stats)
+library(slider)
 
 data <- read.csv("WBE_ND_fig_2_e_2.csv")
 threshold <- 1.0 #threshold to selecting data on quantified concentration
@@ -105,4 +105,68 @@ data_result_xx_2 <- rbind(data_result_xx_2, data_result_xx)
 
 data_result_2 <- mutate(data_result_xx_2, error = abs(logWW - med_3_a))
 
+
+#------------------------------------------------------------------------------------------------------------------------------Data aggregation
+data_xx_1 <- read.csv("20260122_Figure2_1.csv") #result: r:1:5 (Line 33)
+data_xx_2 <- read.csv("20260122_Figure2_2.csv") #result: r:6:10
+data_xx_3 <- read.csv("20260122_Figure2_3.csv") #result: r:11:15
+data_xx_4 <- read.csv("20260122_Figure2_4.csv") #result: r:16:20
+
+data_xx_result <- NULL
+
+#-------------------------------------------------
+data_xx <- data_xx_4
+for(k in 1:5){
+  start <-  1+55*(k-1)
+  end <- 55*k
+  data_xx_a <- data_xx[start:end,]
+  #data_xx_a <- data_xx[1:55,]
+  
+  data_xx_b <- data_xx_a %>% mutate(
+    ND_total = slide_dbl(
+      ND_2,
+      ~ sum(.x, na.rm = TRUE),
+      .before = 6,
+      .after  = -1,
+      .complete = TRUE
+    )
+  )
+  
+  data_xx_c <- data_xx_b[7:nrow(data_xx_b),]
+  data_xx_result <- rbind(data_xx_result, data_xx_c)
+}
+
+write.csv(data_xx_result, file = "C:/WBE_ND/20260122_Figure2e_result_estimationerror_siumaltionanalysis.csv")
+
+
+
+
+#-----------------------------------------------------------------------------------------------------------------------Data analysis
+data_result <- read.csv("20260122_Figure2e_result_estimationerror_siumaltionanalysis.csv")
+data_result <- data_result %>% select(time, logWW, ND, ND_2, low_3_a, med_3_a, upr_3_a, error, ND_total)
+
+
+data_result_0 <- data_result %>% filter(ND_2 >= 1, ND_total < 1)
+data_result_1 <- data_result %>% filter(ND_2 >= 1, ND_total >= 1)
+data_result_2 <- data_result %>% filter(ND_2 >= 1, ND_total >= 2)
+
+mean(data_result_0$error)
+median(data_result_0$error)
+
+mean(data_result_1$error)
+median(data_result_1$error)
+
+mean(data_result_2$error)
+median(data_result_2$error)
+
+
+#--------------------
+error_size <- 0.20
+data_result_0_a <- data_result_0 %>% filter(error > error_size)
+data_result_1_a <- data_result_1 %>% filter(error > error_size)
+data_result_2_a <- data_result_2 %>% filter(error > error_size)
+
+nrow(data_result_0_a)/nrow(data_result_0)*100
+nrow(data_result_1_a)/nrow(data_result_1)*100
+nrow(data_result_2_a)/nrow(data_result_2)*100
 
